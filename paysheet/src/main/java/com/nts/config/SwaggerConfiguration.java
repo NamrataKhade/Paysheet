e338782d645683beb4f5fc3cdea572573f6997db
Pratiksha1999 <pratiksha.lohar@neutrinotechlabs.com>
2022-05-23 15:16:08 +0530

    permission-screen-api

--- paysheet/src/main/java/com/nts/config/SwaggerConfiguration.java
+++ paysheet/src/main/java/com/nts/config/SwaggerConfiguration.java
@@ -13,41 +13,28 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration implements WebMvcConfigurer {
-	
-	 @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry
-                .addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
-    @Bean
-    public Docket apiDocket() {
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(getApiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nts.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
+	@Bean
+	public Docket apiDocket() {
 
-    private ApiInfo getApiInfo() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.nts.controller")).paths(PathSelectors.any()).build();
+	}
 
-        return new ApiInfoBuilder()
-                .title("Swagger API Doc")
-                .description("More description about the API")
-                .version("1.0.0")
-                .build();
-    }
+	private ApiInfo getApiInfo() {
+
+		return new ApiInfoBuilder().title("Swagger API Doc").description("More description about the API")
+				.version("1.0.0").build();
+	}
 }
